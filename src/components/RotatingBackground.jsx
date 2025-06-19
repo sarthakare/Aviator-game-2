5e31ad15aa2354d3a9796f13c0fc20e8fb6b40d4
Sarthak <thakresarthak6@gmail.com>
2025-06-19 10:26:06 +0530

    added breating effect

--- src/components/RotatingBackground.jsx
+++ src/components/RotatingBackground.jsx
@@ -4,12 +4,21 @@ import "../styles/RotatingBackground.css";
 
 export default function RotatingBackground({ shouldRotate }) {
   return (
-    <div className="h-full w-full relative overflow-hidden">
+    <div className="h-full w-full relative overflow-hidden bg-[#1B1C1D]">
+      {/* Rotating image */}
       <img
         src={background}
         alt="background"
         className={`rotating-bg-img${shouldRotate ? " spin-animate" : ""}`}
       />
+
+      {/* Breathing glowing ellipse (only when shouldRotate is true) */}
+      {shouldRotate && (
+        <div
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                     w-1/2 h-1/2 rounded-full blur-3xl animate-gradient-blur"
+        />
+      )}
     </div>
   );
 }
